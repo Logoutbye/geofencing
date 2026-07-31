482e1d19d90a40eee93df0e29b665a7f031d0520
Logoutbye <muhammadbilal03478@gmail.com>
2026-08-01 00:56:22 +0500

    congifured ios side now needs testing;

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,20 +1,35 @@
 import Flutter
 import UIKit
 import native_geofence
+import flutter_local_notifications
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+
     // Used by plugin: native_geofence
-    // Registers the plugin so it can run in a background/terminated-app context.
     NativeGeofencePlugin.setPluginRegistrantCallback { registry in
       GeneratedPluginRegistrant.register(with: registry)
     }
 
+    // Used by flutter_local_notifications
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { registry in
+      GeneratedPluginRegistrant.register(with: registry)
+    }
+
     GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self
+    }
+
+    return super.application(
+      application,
+      didFinishLaunchingWithOptions: launchOptions
+    )
   }
 }
